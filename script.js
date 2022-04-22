d21d102fc383f9117d872f141cb769d818e145ef
Arthur Van Passel <arthur.vanpassel@internetarchitects.be>
2022-04-22 14:41:29 +0200

    details

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 var time_diff = 30000 // 30 sec
 // console.log(chrome.sessions.device);
 
+// HELPER FUNCTIONS
+function timeDifference(current, previous) {
+	var msPerMinute = 60 * 1000;
+	var msPerHour = msPerMinute * 60;
+	var msPerDay = msPerHour * 24;
+	var msPerMonth = msPerDay * 30;
+	var msPerYear = msPerDay * 365;
+
+	var elapsed = current - previous;
+
+	if (elapsed < msPerMinute) {
+		return Math.round(elapsed / 1000) + ' seconds ago';
+	} else if (elapsed < msPerHour) {
+		return Math.round(elapsed / msPerMinute) + ' minutes ago';
+	} else if (elapsed < msPerDay) {
+		return Math.round(elapsed / msPerHour) + ' hours ago';
+	} else if (elapsed < msPerMonth) {
+		return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+	} else if (elapsed < msPerYear) {
+		return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+	} else {
+		return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+	}
+}
+
 $("body").prepend("<div class='login-info waiting'><div class='lds-roller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>")
 var div = $(".login-info");
 $.get("https://sleepy-meadow-58897.herokuapp.com/accounts")
@@ -19,14 +44,21 @@ $.get("https://sleepy-meadow-58897.herokuapp.com/accounts")
 				const element = data.message[i];
 				const d = new Date();
 				let time = d.getTime();
-				console.log(i,time - element.last_update)
+				console.log(i, time - element.last_update)
+				console.log(i, timeDifference(time, element.last_update))
 				var still_active = element.active && time - element.last_update <= time_diff;
 
 				var html = `
 					<div class="login-item" id="${i}">
-						<div class="status ${still_active ? "inUse" : "available"}">${still_active ? "IN USE" : "AVAILABLE"}</div>
-						<div class="email">${element.login}</div>
-						<a class="fillIn" href="#">Fill in</a>
+						<div class="top">
+							<div class="status ${still_active ? "inUse" : "available"}">${still_active ? "IN USE" : "AVAILABLE"}</div>
+							<div class="email">${element.login}</div>
+							<a class="fillIn" href="#">Fill in</a>
+						</div>
+						<details>
+						<summary></summary>
+							<div class="time-info"><strong>Last active: </strong>${timeDifference(time, element.last_update)}</div>
+						</details>
 					</div>
 				`
 				div.append(html);
@@ -35,14 +67,18 @@ $.get("https://sleepy-meadow-58897.herokuapp.com/accounts")
 			$(".login-info .fillIn").on('click', function (e) {
 				e.preventDefault();
 				const current = this
-				var index = $(current).parent(".login-item").attr("id")
+				var index = $(current).parents(".login-item").attr("id")
 				var intervalScroll = window.setInterval(function putFunction() {
-					$.ajax({url: "https://sleepy-meadow-58897.herokuapp.com/accounts/"+index+"/active", method: "PUT"})
+					$.ajax({
+							url: "https://sleepy-meadow-58897.herokuapp.com/accounts/" + index + "/active",
+							method: "PUT"
+						})
 						.fail(function (data) {
 							div.addClass("error");
 							div.prepend(`<span>${data.status} ${data.statusText}</span>`);
 						})
 						.done(function (data) {
+							console.log("put done",data)
 							$(current).siblings(".status").removeClass("available").addClass("using").text("USING")
 						});
 					return putFunction;
