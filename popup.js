e4b7e452f50e1e8f3835f0a5032acc464393c790
Arthur Van Passel <arthur.vanpassel@internetarchitects.be>
2022-04-25 17:36:18 +0200

    username update

--- popup.js
+++ popup.js
@@ -1,27 +1,10 @@
 // When the button is clicked, inject setPageBackgroundColor into current page
 chrome.storage.sync.get("username", (data) => {
   let username = data.username;
-  console.log(username)
   document.querySelector("#userName").setAttribute('value', username)
   document.querySelector("#submitName").addEventListener("click", function () {
-    console.log(this)
-    // let [tab] = await chrome.tabs.query({
-    //   active: true,
-    //   currentWindow: true
-    // });
-  
-    // chrome.scripting.executeScript({
-    //   target: {
-    //     tabId: tab.id
-    //   },
-    //   function: function setPageBackgroundColor() {
-    //     // The body of this function will be executed as a content script inside the current page
-    //     chrome.storage.sync.get("color", ({
-    //       color
-    //     }) => {
-    //       document.body.style.backgroundColor = color;
-    //     });
-    //   },
-    // });
+    username = document.querySelector("#userName").value
+    chrome.storage.sync.set({ username });
+    console.log('Random username set to', `username: ${username}`);
   });
 });
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // CONFIG
 var time_diff = 30000 // 30 sec
-var url = "https://sleepy-meadow-58897.herokuapp.com"
-var url = "http://localhost:5000"
+var url = "https://sleepy-meadow-58897.herokuapp.com";
+let username, intervalUpdate;
+chrome.storage.sync.get("username", (data) => {
+	username = data.username;
+});
+// var url = "http://localhost:5000"
 // console.log(chrome.sessions.device);
 
 // HELPER FUNCTIONS
@@ -33,6 +37,21 @@ function timeDifference(current, previous) {
 	}
 }
 
+function startUpdateUser(index) {
+	intervalUpdate = window.setInterval(function () {
+		$.ajax({
+				url: url + "/accounts/" + index + "/update",
+				method: "PUT"
+			})
+			.fail(function (data) {
+				console.log(data.status, data.statusText);
+			})
+			.done(function (data) {
+				console.log("put update done", data)
+			});
+	}, time_diff);
+}
+
 $("body").prepend("<div class='login-info waiting'><div class='lds-roller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>")
 var div = $(".login-info");
 $.get(url + "/accounts")
@@ -44,80 +63,94 @@ $.get(url + "/accounts")
 		div.prepend(`<span>${data.status} ${data.statusText}</span>`);
 	})
 	.done(function (data) {
-		console.log("done", data)
 		if (data.status == "OK") {
 			for (let i = 0; i < data.message.length; i++) {
 				const element = data.message[i];
 				const d = new Date();
 				let time = d.getTime();
-				console.log(i, time - element.last_update)
-				console.log(i, timeDifference(time, element.last_update))
 				var still_active = time - element.last_update <= time_diff;
-
-				var html = `
-					<div class="login-item" id="${i}">
-						<div class="top">
-							<div class="status ${still_active ? "inUse" : "available"}">${still_active ? "IN USE" : "AVAILABLE"}</div>
-							<div class="email">${element.login}</div>
-							<a class="fillIn" href="#">Fill in</a>
-						</div>
-						<details>
-						<summary></summary>
-				`;
-				if (still_active) {
-					html += `
+				var current_user_active = still_active && element.user == username
+				var details = "",
+					status = "",
+					statusText = "",
+					action = "";
+				if (current_user_active) {
+					status = "using";
+					statusText = "USING";
+					details = `
+						<div class="user"><strong>Currently using: </strong>${element.user}</div>
+						<div class="time-info"><strong>Using since: </strong>${timeDifference(time, element.last_login)}</div>
+					`;
+					action = `<a class="action endSession" href="#">End session</a>`
+					startUpdateUser(i);
+				} else if (still_active) {
+					status = "inUse";
+					statusText = "IN USE";
+					details = `
 						<div class="user"><strong>Currently using: </strong>${element.user}</div>
 						<div class="time-info"><strong>Using since: </strong>${timeDifference(time, element.last_login)}</div>
 					`
 				} else {
-					html += `<div class="time-info"><strong>Last active: </strong>${timeDifference(time, element.last_update)}</div>`
+					status = "available";
+					statusText = "AVAILABLE";
+					details = `
+						<div class="user"><strong>Last user: </strong>${element.user}</div>
+						<div class="time-info"><strong>Last active: </strong>${timeDifference(time, element.last_update)}</div>
+					`
+					action = `<a class="action fillIn" href="#">Fill in</a>`
 				}
-				html += `
+
+
+				var html = `
+					<div class="login-item" id="${i}">
+						<div class="top">
+							<div class="status ${status}">${statusText}</div>
+							<div class="email">${element.login}</div>
+							${action}
+						</div>
+						<details>
+							<summary></summary>
+							${details}
 						</details>
 					</div>
 				`
 				div.append(html);
 			}
 
-			$(".login-info .fillIn").on('click', function (e) {
+			$(".login-info .action").on('click', function (e) {
 				e.preventDefault();
 				const current = this
-				var index = $(current).parents(".login-item").attr("id");
-				$.ajax({
-						url: url + "/accounts/" + index + "/active",
-						method: "PUT",
-						data: {
-							user: "Arthur"
-						}
-					})
-					.fail(function (data) {
-						div.addClass("error");
-						div.prepend(`<span>${data.status} ${data.statusText}</span>`);
-					})
-					.done(function (data) {
-						console.log("put active done", data)
-						$(current).siblings(".status").removeClass("available").addClass("using").text("USING")
-					});
-				var intervalScroll = window.setInterval(function () {
+				if ($(current).hasClass('fillIn')) {
+					var index = $(current).parents(".login-item").attr("id");
 					$.ajax({
-							url: url + "/accounts/" + index + "/update",
-							method: "PUT"
+							url: url + "/accounts/" + index + "/active",
+							method: "PUT",
+							data: {
+								user: username
+							}
 						})
 						.fail(function (data) {
-							console.log(data.status, data.statusText);
+							div.addClass("error");
+							div.prepend(`<span>${data.status} ${data.statusText}</span>`);
 						})
 						.done(function (data) {
-							console.log("put update done", data)
+							console.log("put active done", data)
+							$(current).siblings(".status").removeClass("available").addClass("using").text("USING");
+							$(current).removeClass("fillIn").addClass("endSession").text("End session");
+							startUpdateUser(index);
 						});
-				}, time_diff);
+				} 
+				else if ($(current).hasClass('endSession')) {
+					console.log("end session")
+					var index = $(current).parents(".login-item").attr("id");
+					clearInterval(intervalUpdate);
+					$(current).siblings(".status").removeClass("using").addClass("available").text("AVAILABLE");
+					$(current).removeClass("endSession").addClass("fillIn").text("Fill in");
+				}
 			})
+
 		} else {
 			div.addClass("error");
 			div.prepend(`<span>${data.message}</span>`);
 		}
 	});
-
-
-// fetch(url + '/accounts/1/active', {method: "PUT"})
-// 	.then(response => response.json())
-// 	.then(data => console.log(data));
